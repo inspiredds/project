b9824822e9798ee976214460256c7229e98b5553
Alexandr <example@gmail.com>
2021-08-23 06:10:45 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,36 @@
+"use sctrict";
+
+/* alert('Hello');
+
+const result = confirm('Are u here?');
+console.log(result);
+
+const answer = prompt('Вам есть 18?', '1');
+console.log(answer);
+console.log(typeof(answer)); */
+
+const answers = [];
+
+answers[0] = prompt('Вам есть 18?', '');
+answers[1] = prompt('Ваше  имя?', '');
+answers[2] = prompt('Фамилия?', '');
+
+document.write(answers);
+
+/* let number = 5;
+
+const width = 1;
+
+console.log(number/2);
+
+const man = {
+    name: "Alex",
+    age: 23,
+    isMarried: false
+};
+
+console.log(man.name);
+console.log(man["age"]);
+
+let arr = ["plum.png", "orange.jpg", 7, "apple.bmp", {name: "spyashij"}, []];
+console.log(arr[4]["name"]); */
