fd2dab2d3ecd5affe47b9b5afea10eda21c1cf3e
Alexandr <example@gmail.com>
2021-09-05 06:38:56 +0300

    Массивы и условия

--- js/script2.js
+++ js/script2.js
@@ -1,5 +1,3 @@
-"use sctrict";
-
 const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?",'');
 
 const personalMovieDB = {
@@ -10,8 +8,57 @@ const personalMovieDB = {
     privat: false
 };
 
-const filmTitle = prompt('Один из просмотренных фильмов?','');
+for (let i = 0; i < personalMovieDB.count; i++) {
+    const filmTitle = prompt('Один из просмотренных фильмов?',''), 
+          filmValue = prompt('На сколько оцените его?','');
 
-personalMovieDB.movies[filmTitle] = prompt('На сколько оцените его?','');
+    if (filmTitle.length == 0 || filmValue.length == 0 || 
+        filmTitle.length > 50 || filmValue.length > 50 || filmTitle == null || filmValue == null) {
+        alert('Неверное название фильма или оценка. Введите еще раз.');
+        i--;
+    } else {
+        personalMovieDB.movies[filmTitle] = filmValue;
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    alert('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    alert('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+    alert('Вы киноман');
+}
 
 console.log(personalMovieDB);
+
+// while и doWhile
+
+/* let i2 = 0;
+
+while (i2 < personalMovieDB.count) {
+    const filmTitle = prompt('Один из просмотренных фильмов?',''), 
+          filmValue = prompt('На сколько оцените его?','');
+
+    if (filmTitle.length == 0 || filmValue.length == 0 || 
+        filmTitle.length > 50 || filmValue.length > 50 || filmTitle == null || filmValue == null) {
+        alert('Неверное название фильма или оценка. Введите еще раз.');
+    } else {
+        personalMovieDB.movies[filmTitle] = filmValue;
+        i2++;
+    }
+}
+
+i2 = 0;
+
+do {
+    const filmTitle = prompt('Один из просмотренных фильмов?',''), 
+          filmValue = prompt('На сколько оцените его?','');
+
+    if (filmTitle.length == 0 || filmValue.length == 0 || 
+        filmTitle.length > 50 || filmValue.length > 50 || filmTitle == null || filmValue == null) {
+        alert('Неверное название фильма или оценка. Введите еще раз.');
+    } else {
+        personalMovieDB.movies[filmTitle] = filmValue;
+        i2++;
+    }
+} while (i2 < personalMovieDB.count); */
---
+++ js/script3.js
@@ -0,0 +1,57 @@
+// Условия
+
+/* if (4 == 9) {
+    console.log('Ok!');
+} else {
+    console.log('Error');
+}
+
+const num = 50;
+
+if (num < 49) {
+    console.log('Error');
+} else if (num > 100) {
+    console.log('Mnogo');
+} else {
+    console.log('Ok!');
+}
+
+(num === 50) ? console.log('Ok!') : console.log('Error');
+
+switch (num) {
+    case 49:
+        console.log('Neverno!');
+        break;
+    case 100:
+        console.log('Neverno!');
+        break;
+    case 50:
+        console.log('Verno!');
+        break;
+    default:
+        console.log('Ne v etot raz!');
+        break;
+} */
+    
+//циклы
+
+/* let num = 50;
+
+while (num < 55) {
+    console.log(num);
+    num++;
+}
+
+do {
+    console.log(num);
+    num++;
+} while (num < 55);
+
+for (let i = 1; i < 10; i++) {
+    if (i == 6) {
+        // break;
+        continue;
+    }
+    console.log(i);
+} */
+
