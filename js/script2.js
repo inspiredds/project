e66fbe8c8ff793cdef8a13553f77de6e4be1510f
Alexandr <example@gmail.com>
2021-09-08 10:59:47 +0300

    prototypes and practice

--- js/script2.js
+++ js/script2.js
@@ -1,54 +1,70 @@
-let numberOfFilms;
-
-function start() {
-    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?",'');
-}
 
 const personalMovieDB = {
-    count: numberOfFilms,
+    count: '',
     movies: {},
     actors: {},
     genres: [],
-    privat: false
-};
-
-function rememberMyFilms () {
-    for (let i = 0; i < personalMovieDB.count; i++) {
-        const filmTitle = prompt('Один из просмотренных фильмов?',''), 
-              filmValue = prompt('На сколько оцените его?','');
-    
-        if (filmTitle.length == 0 || filmValue.length == 0 || 
-            filmTitle.length > 50 || filmValue.length > 50 || 
-            filmTitle == null || filmValue == null) {
-            alert('Неверное название фильма или оценка. Введите еще раз.');
-            i--;
+    privat: false,
+    start: function() {
+        while (personalMovieDB.count == null || personalMovieDB.count === '' ||
+               isNaN(personalMovieDB.count)) {
+            personalMovieDB.count = 
+            +prompt("Сколько фильмов вы уже посмотрели?",''); 
+        }
+    },
+    rememberMyFilms: function() {
+        for (let i = 0; i < personalMovieDB.count; i++) {
+            const filmTitle = prompt('Один из просмотренных фильмов?',''), 
+                  filmValue = prompt('На сколько оцените его?','');
+        
+            if (filmTitle == null || filmValue == null ||
+                filmTitle == '' || filmValue == '' || 
+                filmTitle.length > 50 || filmValue.length > 50) {
+                alert('Неверное название фильма или оценка. Введите еще раз.');
+                i--;
+            } else {
+                personalMovieDB.movies[filmTitle] = filmValue;
+            }
+        }
+    },
+    detectPersonalLever: function() {
+        if (personalMovieDB.count < 10) {
+            alert('Просмотрено довольно мало фильмов');
+        } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+            alert('Вы классический зритель');
+        } else if (personalMovieDB.count >= 30) {
+            alert('Вы киноман');
+        }
+    },
+    showMyDB: function() {
+        if (personalMovieDB.privat == false) {
+            console.log(personalMovieDB.movies);
+        }
+    },
+    writeYourGenres: function() {
+        for (let i = 1; i <= 3; i++) {
+            const preGenre = prompt(`Ваш любимый жанр под номером ${i}`, '');
+            if (preGenre == null || preGenre.length == 0) {
+                console.log('Неверное название жанра');
+                i--;
+            } else {
+                personalMovieDB.genres[i - 1] = preGenre;   
+            }
+        }
+        personalMovieDB.genres.forEach((item, i) => {
+            console.log(`Любимый жанр ${i + 1} - это ${item}`);
+        });
+    },
+    toggleVisibleMyDB: function() {
+        if (personalMovieDB.privat) {
+            personalMovieDB.privat = false;   
         } else {
-            personalMovieDB.movies[filmTitle] = filmValue;
+            personalMovieDB.privat = true;
         }
     }
-}
+};
 
-function detectPersonalLever() {
-    if (personalMovieDB.count < 10) {
-        alert('Просмотрено довольно мало фильмов');
-    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-        alert('Вы классический зритель');
-    } else if (personalMovieDB.count >= 30) {
-        alert('Вы киноман');
-    }
-}
+personalMovieDB.start();
+personalMovieDB.writeYourGenres();
 
 console.log(personalMovieDB);
-
-function showMyDB () {
-    if (personalMovieDB.privat == false) {
-        console.log(personalMovieDB.movies);
-    }
-}
-
-function writeYourGenres () {
-    for (let i = 1; i <= 3; i++) {
-        personalMovieDB.genres[i-1] =
-         prompt(`Ваш любимый жанр под номером ${i}`, '');
-    }
-}
---
+++ js/script6.js
@@ -0,0 +1,25 @@
+
+let str = 'some';
+let strObj = new String(str);
+
+// console.log(typeof(str));
+// console.log(typeof(strObj));
+
+console.dir([1, 2, 3]);
+
+const soldier = {
+    health: 400,
+    armor: 100
+};
+
+const john = Object.create(soldier);
+
+/* const john = {
+    health: 100
+};
+
+// john.__proto__ = soldier;
+
+Object.setPrototypeOf(john, soldier);
+
+console.log(john.armor); */
